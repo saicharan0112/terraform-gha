5283bf131e6828c98311df52eb8ece550f38002c
Sai Charan <saicharan00112@gmail.com>
2023-04-02 18:25:28 +0530

    change image

--- main.tf
+++ main.tf
@@ -20,11 +20,10 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
-  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-stable"
+      image = "debian-cloud/debian-11"
     }
   }
 
